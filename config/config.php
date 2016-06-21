94e0b1f02571e59dd1d8c2dad52461fcd8baadec
Lukas Reschke <lukas@owncloud.com>
2016-06-21 12:10:30 +0200

    Initial import

---
+++ config/config.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @license MIT <http://opensource.org/licenses/MIT>
+ */
+
+/**
+ * Welcome to the almighty configuration file. In this file the update definitions for each version are released. Please
+ * make sure to read below description of the config format carefully before proceeding.
+ *
+ * ownCloud updates are delivered by a release channel, at the moment the following channels are available:
+ *
+ * - production
+ * - stable
+ * - beta
+ * - daily
+ *
+ * With exception of daily (which is a daily build of master) all of them need to be configured manually. The config
+ * array looks like the following:
+ *
+ * 'production' => [
+ * 	'8.2' => [
+ * 		'latest' => '8.2.3',
+ * 		'web' => 'https://docs.nextcloud.org/server/9/admin_manual/maintenance/upgrade.html',
+ * 		// downloadUrl is an optional entry, if not specified the URL is generated using https://download.nextcloud.com/server/releases/nextcloud-'.$newVersion['latest'].'.zip
+ * 		'downloadUrl' => 'https://download.nextcloud.com/foo.zip',
+ * 	],
+ * ]
+ *
+ * In this case if a Nextcloud with the major release of 8.2 sends an update request the 8.2.3 version is returned if the
+ * current Nextcloud version is below 8.2.
+ *
+ * The search for releases in the config array is fuzzy and happens as following:
+ * 	1. Major.Minor.Maintenance.Revision
+ * 	2. Major.Minor.Maintenance
+ * 	3. Major.Minor
+ * 	4. Major
+ *
+ * Once a result has been found this one is taken. This allows it to define an update order in case some releases should
+ * not be skipped. Let's take a look at an example:
+ *
+ * 'production' => [
+ * 	'8.2.0' => [
+ * 		'latest' => '8.2.1',
+ * 		'web' => 'https://docs.nextcloud.org/server/8.2/admin_manual/maintenance/upgrade.html',
+ * 	],
+ * 	'8.2' => [
+ * 		'latest' => '8.2.4',
+ * 		'web' => 'https://docs.nextcloud.org/server/8.2/admin_manual/maintenance/upgrade.html',
+ * 	],
+ * 	'8.2.4' => [
+ * 		'latest' => '9.0.0',
+ * 		'web' => 'https://docs.nextcloud.org/server/8.2/admin_manual/maintenance/upgrade.html',
+ * 	],
+ * ]
+ *
+ * This configuration array would have the following meaning:
+ *
+ * 1. 8.2.0 instances would be delivered 8.2.1
+ * 2. All instances below 8.2.4 EXCEPT 8.2.1 would be delivered 8.2.4
+ * 3. 8.2.4 instances get 9.0.0 delivered
+ *
+ * Oh. And be a nice person and also adjust the integration tests at /tests/integration/features/update.feature after doing
+ * a change to the update logic. That way you can also ensure that your changes will do what you wanted them to do. The
+ * tests are automatically executed on Travis or you can do it locally:
+ *
+ * 	- php -S localhost:8888 update-server/index.php &
+ * 	- tests/integration/ && ../../vendor/bin/behat .
+ */
+
+return [
+	'production' => [
+		'9.0' => [
+			'latest' => '9.0.51',
+			'web' => 'https://docs.nextcloud.org/server/9/admin_manual/maintenance/upgrade.html',
+		],
+	],
+	'stable' => [
+		'9.0' => [
+			'latest' => '9.0.51',
+			'web' => 'https://docs.nextcloud.org/server/9/admin_manual/maintenance/upgrade.html',
+		],
+	],
+	'beta' => [
+			'9.0' => [
+			'latest' => '9.0.51',
+			'web' => 'https://docs.nextcloud.org/server/9/admin_manual/maintenance/upgrade.html',
+		],
+	],
+	'daily' => [
+		'9.0' => [
+			'downloadUrl' => 'https://download.nextcloud.com/server/daily/latest.zip',
+			'web' => 'https://docs.nextcloud.org/server/9/admin_manual/maintenance/upgrade.html',
+		],
+	],
+];
---
+++ tests/integration/features/update.feature
@@ -0,0 +1,63 @@
+Feature: Testing the update scenario of releases
+
+  Scenario: Updating an outdated Nextcloud 9.0.50 on the production channel
+    Given There is a release with channel "production"
+    And The received version is "9.0.50"
+    When The request is sent
+    Then The response is non-empty
+    And Update to version "9.0.51" is available
+    And URL to download is "https://download.nextcloud.com/server/releases/nextcloud-9.0.51.zip"
+    And URL to documentation is "https://docs.nextcloud.org/server/9/admin_manual/maintenance/upgrade.html"
+
+  Scenario: Updating an outdated ownCloud 9.0.50 on the stable channel
+    Given There is a release with channel "stable"
+    And The received version is "9.0.50"
+    When The request is sent
+    Then The response is non-empty
+    And Update to version "9.0.51" is available
+    And URL to download is "https://download.nextcloud.com/server/releases/nextcloud-9.0.51.zip"
+    And URL to documentation is "https://docs.nextcloud.org/server/9/admin_manual/maintenance/upgrade.html"
+
+  Scenario: Updating an outdated ownCloud 9.0.50 on the beta channel
+    Given There is a release with channel "beta"
+    And The received version is "9.0.50"
+    When The request is sent
+    Then The response is non-empty
+    And Update to version "9.0.51" is available
+    And URL to download is "https://download.nextcloud.com/server/releases/nextcloud-9.0.51.zip"
+    And URL to documentation is "https://docs.nextcloud.org/server/9/admin_manual/maintenance/upgrade.html"
+
+  Scenario: Updating an up-to-date Nextcloud 9.0.51 on the production channel
+    Given There is a release with channel "production"
+    And The received version is "9.0.51"
+    When The request is sent
+    Then The response is empty
+
+  Scenario: Updating an up-to-date Nextcloud 9.0.51 on the stable channel
+    Given There is a release with channel "stable"
+    And The received version is "9.0.51"
+    When The request is sent
+    Then The response is empty
+
+  Scenario: Updating an up-to-date Nextcloud 9.0.51 on the beta channel
+    Given There is a release with channel "beta"
+    And The received version is "9.0.51"
+    When The request is sent
+    Then The response is empty
+
+  Scenario: Updating an outdated-dated Nextcloud 9.0 daily
+    Given There is a release with channel "daily"
+    And The received version is "9.0.100"
+    And the received build is "2012-10-19T18:44:30+00:00%208ee2009de36e01a9866404f07722892f84c16e3e"
+    When The request is sent
+    Then The response is non-empty
+    And Update to version "100.0.0.0" is available
+    And URL to download is "https://download.nextcloud.com/server/daily/latest.zip"
+    And URL to documentation is "https://docs.nextcloud.org/server/9/admin_manual/maintenance/upgrade.html"
+
+  Scenario: Updating an up-to-date Nextcloud 9.0 daily
+    Given There is a release with channel "daily"
+    And The received version is "9.0.100"
+    And the received build is "2019-10-19T18:44:30+00:00"
+    When The request is sent
+    Then The response is empty
