751552309a51fb997c1d55db9b979d0399d6efe5
Morris Jobke <hey@morrisjobke.de>
2018-09-28 14:24:00 +0200

    Update stable14 production instances to latest stable14 release
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- config/config.php
+++ config/config.php
@@ -105,6 +105,22 @@
 
 return [
 	'production' => [
+		'14' => [
+			'100' => [
+				'latest' => '14.0.1',
+				'internalVersion' => '14.0.1.1',
+				'downloadUrl' => 'https://download.nextcloud.com/server/releases/nextcloud-14.0.1.zip',
+				'web' => 'https://docs.nextcloud.com/server/14/admin_manual/maintenance/upgrade.html',
+				'eol' => false,
+				'minPHPVersion' => '7.0',
+				'signature' => 'GMLD/dgAkP8AtldfrBib1Jz9WAehw3wqnCRfReCckOt5XfGY8DjtGzDuyt285862
+8wOPvmEIZsrGSooGiAgNv4H3kXO21EzzBwOyov26dyh+OtTxfxpN6yLEKpcRSWPj
+GweHorjisB2gqf6P/nD9yo69QCEIZKm8O2wx09K+QC8jwJ+UxdSm6p7b/d14lPwW
+n6hwHIcpwKicNJiLGWhHpslC64nIqp+DAbOeFtl+mVGpigyNec5+JekMVCayAGAs
+RS5Otchsk2GtWqPWtQEWSbkPFxuIJY9ij1RY+ocABIfQ8b55pbwkRNpjAawq5+3G
+UhPQ296yv/FbIxF+rWpL+g==',
+			],
+		],
 		'13' => [
 			'100' => [
 				'latest' => '13.0.6',
