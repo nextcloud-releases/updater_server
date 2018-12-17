89f34c212409c03f0706b7bdc4d10cb0cc807074
Jos Poortvliet <jos@opensuse.org>
2018-12-17 14:52:20 +0100

    Give 10% of 14 users Nc 15
    10% on 14 gets 15

--- config/config.php
+++ config/config.php
@@ -271,7 +271,7 @@ fq4UospVzAmRe4UGPTUiqQ==',
 			],
 		],
 		'14' => [
-			'100' => [
+            '90' => [
 				'latest' => '14.0.4',
 				'internalVersion' => '14.0.4.2',
 				'downloadUrl' => 'https://download.nextcloud.com/server/releases/nextcloud-14.0.4.zip',
@@ -279,12 +279,26 @@ fq4UospVzAmRe4UGPTUiqQ==',
 				'eol' => false,
 				'minPHPVersion' => '7.0',
 				'signature' => 'g7NKa7ZQLKfBAYOIEA0SHochEpYiCEiqlKdh5qlqjJu+QAA4kGyJ+J26RiB5gm56
-KeqkSp347NaR68U6Qb9eaC3V1rrQdd990KncXqfmbqZVVN/8DXI6mej9Y5lv3Pop
-9J8QuN6ViN3c3hFG/jf9laTdsLI9UCoUOM5LvngJbs2Sjv+vmMMkDguE+XyD5i9d
-6Cq2ia0CEVY5J7RpBmvEaUOFSi+2fxn+QLT467pkQL9yt45Acq0oXrxXdbW9PMYP
-N83+1F/AaXgMeOVJ08yffH1r0KpvN07xHAkepfPLBxe/8KrBLLJ6QIcH0qD9cJOc
-cFXWg2ZhXYGHpfMvrMtueg==',
+                                KeqkSp347NaR68U6Qb9eaC3V1rrQdd990KncXqfmbqZVVN/8DXI6mej9Y5lv3Pop
+                                9J8QuN6ViN3c3hFG/jf9laTdsLI9UCoUOM5LvngJbs2Sjv+vmMMkDguE+XyD5i9d
+                                6Cq2ia0CEVY5J7RpBmvEaUOFSi+2fxn+QLT467pkQL9yt45Acq0oXrxXdbW9PMYP
+                                N83+1F/AaXgMeOVJ08yffH1r0KpvN07xHAkepfPLBxe/8KrBLLJ6QIcH0qD9cJOc
+                                cFXWg2ZhXYGHpfMvrMtueg==',
 			],
+            '10' => [
+                'latest' => '15.0.0',
+                'internalVersion' => '15.0.0.10',
+                'downloadUrl' => 'https://download.nextcloud.com/server/releases/nextcloud-15.0.0.zip',
+                'web' => 'https://docs.nextcloud.com/server/15/admin_manual/maintenance/upgrade.html',
+                'eol' => false,
+                'minPHPVersion' => '7.0',
+                'signature' => 'c4llfwhYTKaEiTWVivJ1NgTIS5q2mxJVJkyew0nd/setTzpXt2H9zXKGQLmjUcy7
+                                fSeW5+wkUfGD2J3XrcLKifGZMjxrhtAW97L1g/o8gp84ZO1WbrOKfneTLDXWkmwg
+                                nztf/5z0F0nppOyUX6HR84UhwDbhET7U8JV/1Ik7OO6D361U4sxELUhvg6GyQbdS
+                                oJ/t4MvMe1Fs+F5Q7dZzczivxu5oB0n2cVu9WMh8VnV6MiYcKV4/w7poibHMO16k
+                                4IHl6E2hTF3un0obMKy7SRY4xXJ0Ohmj5Ne/8iDRjM7oop5Tzpnf1nPLEJJOZPSN
+                                fq4UospVzAmRe4UGPTUiqQ==',
+            ],
 		],
 		'13' => [
 			'100' => [
