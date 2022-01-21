209388299101afc2c48a27a03241011c5181d9da
John Molakvoæ <skjnldsv@protonmail.com>
2022-01-21 13:48:25 +0100

    Bump composer
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,4 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Behat\\Testwork' => array($vendorDir . '/behat/behat/src'),
+    'Behat\\Gherkin' => array($vendorDir . '/behat/gherkin/src'),
+    'Behat\\Behat' => array($vendorDir . '/behat/behat/src'),
 );
